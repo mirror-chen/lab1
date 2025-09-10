7ff612e1cec0610582f63b933b01f75ce56264b7
mirror <mirror.chen@mail.utoronto.ca>
2025-09-10 01:01:28 -0400

    multiples.java

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Multiples {
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+}
